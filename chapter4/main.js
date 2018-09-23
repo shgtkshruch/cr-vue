9bc6ba1595c2c5a0dc86d7cced5944ac61845d46
shgtkshruch <shgtk.shruch@gmail.com>
2018-09-23 12:41:50 +0900

    Render fruits list

---
+++ chapter4/main.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+    fruits: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 400 },
+      { id: 4, name: 'おれんじ', price: 300 },
+      { id: 5, name: 'めろん', price: 500 }
+    ]
+  }
+});
